e1f5c9c10724f93aa7edee078699547ff1f907a6
Andrew Lloyd <andrew85.lloyd@gmail.com>
2016-11-23 22:18:37 -0800

    Catch ctrl c on run script and stop/remove containers

--- calc-pi.sh
+++ calc-pi.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "* Stopping and removing containers"
+        docker-compose down
+        exit 0
+}
+
+docker build -t sparktest .
 docker-compose up -d
 docker-compose scale spark_slave=5
-docker-compose logs --follow
+docker-compose logs --follow spark_util
